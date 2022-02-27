ffceeda8d9eca9a44c359e2bbed0068761250633
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-02-28 00:59:13 +0400

    changes in 2nd

--- HomeWork8/2nd.js
+++ HomeWork8/2nd.js
@@ -6,15 +6,18 @@ async function dogList(){
 
     let response = await fetch(`https://dog.ceo/api/breed/${select.options[select.selectedIndex].value}/images`);
 
-    let json = await response.json();
-    let img = document.createElement('img');
+    
+    if (response.status == '200') {
+        let json = await response.json();
+        let img = document.createElement('img');
 
     for (let key in json.message) 
         {
-            
             img.src = json.message[key];
             img.style = 'display:flex;width:500px';
             document.body.appendChild(img);
+        }
+
         }
 }
 
