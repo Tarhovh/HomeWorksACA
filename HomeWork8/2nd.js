e54241a64670d1e622ce923129873f5e544b8aec
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-02-28 00:03:01 +0400

    Changes in 2nd

--- HomeWork8/2nd.js
+++ HomeWork8/2nd.js
@@ -2,23 +2,22 @@
 
 const select = document.getElementById('pet-select')
 
-function dogList(){
+async function dogList(){
 
-select.addEventListener('change', async() => {
+    let response = await fetch(`https://dog.ceo/api/breed/${select.options[select.selectedIndex].value}/images`);
 
-let response = await fetch(`https://dog.ceo/api/breed/${select.options[select.selectedIndex].value}/images`);
+    let json = await response.json();
+    let img = document.createElement('img');
 
-let json = await response.json();
-
-for (let key in json.message) {
-let img = document.createElement('img');
-img.src = json.message[key];
-img.style = 'display:flex;width:500px';
-
-document.body.append(img);
-
+    for (let key in json.message) 
+        {
+            
+            img.src = json.message[key];
+            img.style = 'display:flex;width:500px';
+            document.body.appendChild(img);
+        }
 }
+
+select.addEventListener('change', () => { 
+    dogList();
 })
-}
-
-dogList()
