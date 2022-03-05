04ef83ab394b011c8490db18955f5de28db97d28
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-03-05 17:02:22 +0400

    App1 added

---
+++ ReactHomework9/homework-9/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import DataFetching from './DataFetching';
+// import { useEffect, useState } from 'react';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+     <DataFetching/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactHomework9/homework-9/src/DataFetching.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+
+function DataFetching() {
+
+    const [films, setFilms] = useState([])
+
+    useEffect (() => {
+        axios.get('https://ghibliapi.herokuapp.com/films') 
+        .then(res => {
+            setFilms(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+    return (
+        <div>
+<ol>
+    {
+        films.map(film => <li key={film.id}>{film.director}, "{film.title}", ReleaseDate: {film.release_date} 
+                       <ul>
+                       <li> Description: "{film.description}"</li>
+                       </ul>
+                         
+        </li>)
+
+    }
+</ol>
+        </div>
+    )
+}
+
+export default DataFetching;
